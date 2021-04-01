b8dab53576e60abf531de1452ecc1ea856171484
antykasyahrial <antykass2@gmail.com>
2021-04-01 19:16:09 +0700

    models and migrate

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('username');
+            $table->string('phone');
+            $table->string('email');
+            $table->date('birthDate');
+            $table->string('sex');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
---
+++ laravel/database/migrations/2021_04_01_121425_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->bigInteger('id_user')->unsigned();
+            $table->bigInteger('id_product')->unsigned();
+            $table->integer('qty');
+            $table->integer('totalPrice');
+            $table->timestamps();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_product')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
